2f1474d980766b59e715d2c6aa794007d176ca92
David Maas <git@pixelalchemy.dev>
2024-09-17 06:42:52 -0500

    Simplified logic of arrangement controller and fixed some bugs.
    * Fixed typo that broke the internal representation of inventory -> inventory swaps
    * Removed unnecessary special-casing of handling empty cells
    * Instead of requiring to drag to empty inventory slot, just swap (avoids soft lock situation)
    * Instead of placing replaced board items in inventory slot, just swap (also avoids soft lock situation)

--- Assets/HandMadeGame/Code/ArrangementController.cs
+++ Assets/HandMadeGame/Code/ArrangementController.cs
@@ -75,94 +75,37 @@ public class ArrangementController : MonoBehaviour
     }
 
     public bool UpdateBoardFromInv(Vector2Int pos, int invPos, Image img) {
-        if (pos[0] == -1 || pos[1] == -1) return false; // invalid tile replacement
-        // check if board already has something in that slot
-        // if it does, move it to inventory
-        if (Board[pos[0], pos[1]] != null) {
-            // find first empty inventory slot
-            int emptyInvIndex = -1;
-            for (int i = 0; i < Inventory.Length; i++) {
-                if (i == invPos || Inventory[i] == null) {
-                    emptyInvIndex = i;
-                    break;
-                }
-            }
-            if (emptyInvIndex == invPos) {
-                DummyItem temp2 = Inventory[emptyInvIndex];
-                Inventory[emptyInvIndex] = Board[pos[0], pos[1]];
-                Inventory[invPos] = Board[pos[0], pos[1]];
-                Board[pos[0], pos[1]] = temp2;
-                SyncDisplay();
-                return true;
-            } else {
-                int internalPos = pos[1] + (3 * pos[0]);
-                Inventory[emptyInvIndex] = Board[pos[0], pos[1]];
-                Board[pos[0], pos[1]] = Inventory[invPos];
-                Inventory[invPos] = null;
-                SyncDisplay();
-                return true;
-            }
-        } else { // otherwise, just move it
-            Board[pos[0], pos[1]] = Inventory[invPos];
-            Inventory[invPos] = null;
-            int internalPosAgain = pos[1] + (3 * pos[0]);
-            SyncDisplay();
-            return true;
-        }
+        if (pos[0] == -1 || pos[1] == -1)
+            return false; // invalid tile replacement
+        DummyItem temp = Board[pos[0], pos[1]];
+        Board[pos[0], pos[1]] = Inventory[invPos];
+        Inventory[invPos] = temp;
+        SyncDisplay();
+        return true;
     }
 
     public bool UpdateBoardFromBoard(Vector2Int newPos, int invPos, Vector2Int curPos, Image img) {
-        if (newPos[0] == -1 || newPos[1] == -1) return false; // invalid tile replacement
-        // check if board already has something in that slot
-        // if it does, move it to inventory
-        if (Board[newPos[0], newPos[1]] != null) {
-            // find first empty inventory slot
-            int emptyInvIndex = -1;
-            for (int i = 0; i < Inventory.Length; i++) {
-                if (i == invPos || Inventory[i] == null) {
-                    emptyInvIndex = i;
-                    break;
-                }
-            }
-            int newInternalPos = newPos[1] + (3 * newPos[0]);
-            int curInternalPos = curPos[1] + (3 * curPos[0]);
-            Inventory[emptyInvIndex] = Board[newPos[0], newPos[1]];
-            Board[newPos[0], newPos[1]] = Board[curPos[0], curPos[1]];
-            Board[curPos[0], curPos[1]] = null;
-            SyncDisplay();
-            return true;
-        } else { // otherwise, just move it
-            Board[newPos[0], newPos[1]] = Board[curPos[0], curPos[1]];
-            Board[curPos[0], curPos[1]] = null;
-            int newInternalPos = newPos[1] + (3 * newPos[0]);
-            int curInternalPos = curPos[1] + (3 * curPos[0]);
-            SyncDisplay();
-            return true;
-        }
+        if (newPos[0] == -1 || newPos[1] == -1)
+            return false; // invalid tile replacement
+        DummyItem temp = Board[newPos[0], newPos[1]];
+        Board[newPos[0], newPos[1]] = Board[curPos[0], curPos[1]];
+        Board[curPos[0], curPos[1]] = temp;
+        SyncDisplay();
+        return true;
     }
 
     public bool UpdateInvFromBoard(Vector2Int curPos, int invPos, Image img) {
-        // check if inventory space is full
-        if (Inventory[invPos] != null) return false;
-        // otherwise, move it to the inventory
+        DummyItem temp = Inventory[invPos];
         Inventory[invPos] = Board[curPos[0], curPos[1]];
-        Board[curPos[0], curPos[1]] = null;
+        Board[curPos[0], curPos[1]] = temp;
         SyncDisplay();
         return true;
     }
 
     public bool UpdateInvFromInv(int curInvPos, int newInvPos, Image img) {
-        // check if inventory space is full. if so, swap items
-        if (Inventory[newInvPos] != null) {
-            DummyItem tempInt = Inventory[newInvPos];
-            Inventory[newInvPos] = Inventory[curInvPos];
-            Inventory[newInvPos] = tempInt;
-            SyncDisplay();
-            return true;
-        }
-        // otherwise,  just place it
+        DummyItem temp = Inventory[newInvPos];
         Inventory[newInvPos] = Inventory[curInvPos];
-        Inventory[curInvPos] = null;
+        Inventory[curInvPos] = temp;
         SyncDisplay();
         return true;
     }
